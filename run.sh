2ca254678a20c2ef9a7b570b09a2e8fa8577a142
Tao Wang <twang2218@gmail.com>
2016-07-30 02:28:26 +1000

    使用 Alpine 基础镜像，这样 php, nginx 镜像会更小一些。Swarm 试验中，每个节点都要下载所有镜像，因此能省就省点好了。 使用 twang2218/etcd 代替 quay.io/coreos/etcd，这样对于那些访问 quay.io 存在困难的人会有所帮助 增加 MACHINE_OPTS，方便防止一些有地区特色的参数配置

--- run.sh
+++ run.sh
@@ -10,16 +10,20 @@ if [ -z "${DOCKER_MACHINE_DRIVER}" ]; then
     DOCKER_MACHINE_DRIVER=virtualbox
 fi
 
+# CN_SPECIAL_OPTS="--engine-registry-mirror https://jxus37ac.mirror.aliyuncs.com"
+INSECURE_OPTS="--engine-insecure-registry 192.168.99.0/24"
+# STORAGE_OPTS="--engine-storage-driver overlay2"
+
+MACHINE_OPTS="${STORAGE_OPTS} ${INSECURE_OPTS} ${CN_SPECIAL_OPTS}"
 
 ##############################
 #      Image Management      #
 ##############################
 
-
 function build() {
     # Build images
     docker build -t ${REGISTRY_USER}/lnmp-nginx:latest -f nginx-php/Dockerfile.nginx ./nginx-php
-    docker build -t ${REGISTRY_USER}/lnmp-php:latest -f nginx-php/Dockerfile.php ./nginx-php
+    docker build -t ${REGISTRY_USER}/lnmp-php:latest -f nginx-php/Dockerfile.php-alpine ./nginx-php
     docker build -t ${REGISTRY_USER}/lnmp-mysql:latest -f mysql/Dockerfile ./mysql
 }
 
@@ -53,15 +57,15 @@ function publish() {
 
 function create_store() {
     NAME=$1
-    docker-machine create -d ${DOCKER_MACHINE_DRIVER} ${NAME}
-    eval $(docker-machine env ${NAME})
-    HostIP=$(docker-machine ip ${NAME})
+    docker-machine create -d ${DOCKER_MACHINE_DRIVER} ${MACHINE_OPTS} ${NAME}
+    eval "$(docker-machine env ${NAME})"
+    HostIP="$(docker-machine ip ${NAME})"
     export KVSTORE="etcd://${HostIP}:2379"
     docker run -d \
         -p 4001:4001 -p 2380:2380 -p 2379:2379 \
         --restart=always \
         --name etcd \
-        quay.io/coreos/etcd:v2.3.7 \
+        twang2218/etcd:v2.3.7 \
             --initial-advertise-peer-urls http://${HostIP}:2380 \
             --initial-cluster default=http://${HostIP}:2380 \
             --advertise-client-urls http://${HostIP}:2379,http://${HostIP}:4001 \
@@ -73,7 +77,7 @@ function create_master() {
     NAME=$1
     echo "kvstore is ${KVSTORE}"
     # eth1 on virtualbox, eth0 on digitalocean
-    docker-machine create -d ${DOCKER_MACHINE_DRIVER} \
+    docker-machine create -d ${DOCKER_MACHINE_DRIVER} ${MACHINE_OPTS} \
         --swarm \
         --swarm-discovery=${KVSTORE} \
         --swarm-master \
@@ -86,7 +90,7 @@ function create_node() {
     NAME=$1
     echo "kvstore is ${KVSTORE}"
     # eth1 on virtualbox, eth0 on digitalocean
-    docker-machine create -d ${DOCKER_MACHINE_DRIVER} \
+    docker-machine create -d ${DOCKER_MACHINE_DRIVER} ${MACHINE_OPTS} \
         --swarm \
         --swarm-discovery=${KVSTORE} \
         --engine-opt="cluster-store=${KVSTORE}" \
@@ -97,7 +101,7 @@ function create_node() {
 function create() {
     create_store kvstore
     create_master master
-    for i in $(seq ${SIZE})
+    for i in $(seq 1 ${SWARM_SIZE})
     do
         create_node node${i} &
     done
@@ -105,8 +109,8 @@ function create() {
     wait
 }
 
-function destroy() {
-    for i in $(seq ${SIZE})
+function remove() {
+    for i in $(seq 1 ${SWARM_SIZE})
     do
         docker-machine rm -y node${i} || true
     done
@@ -119,7 +123,7 @@ function destroy() {
 ##############################
 
 function up() {
-    eval $(docker-machine env --swarm master)
+    eval "$(docker-machine env --swarm master)"
     docker-compose up -d
 }
 
@@ -128,7 +132,7 @@ function scale() {
 }
 
 function down() {
-    eval $(docker-machine env --swarm master)
+    eval "$(docker-machine env --swarm master)"
     docker-compose down
 }
 
@@ -141,13 +145,13 @@ function main() {
     shift
     case "${Command}" in
         create)     create ;;
-        destroy)    destroy ;;
+        remove)     remove ;;
         up)         up ;;
-        scale)       scale $@ ;;
+        scale)      scale "$@" ;;
         down)       down ;;
         publish)    publish ;;
-        *)          echo "Usage: $0 <create|destroy|up|scale|down|publish>"; exit 1 ;;
+        *)          echo "Usage: $0 <create|remove|up|scale|down|publish>"; exit 1 ;;
     esac
 }
 
-main $@
+main "$@"
